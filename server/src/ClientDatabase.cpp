9c7057889f7bd68b587ac0b7fef3cf20edbb8e56
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-01-28 17:06:58 +0100

    untear apart stuff

--- server/src/ClientDatabase.cpp
+++ server/src/ClientDatabase.cpp
@@ -10,10 +10,11 @@ ClientDatabase::~ClientDatabase()
 auto ClientDatabase::AddClient(std::unique_ptr<IClient> new_client) -> void
 {
     auto uuid = new_client->GetUUID();
-    IRC::Mutex<IClient> client_mutex(std::move(new_client));
 
-    auto pair = std::make_pair(uuid, std::move(client_mutex));
-    auto ret = clients_.insert(std::move(pair));
+    auto ret = clients_.insert(std::make_pair(
+        uuid,
+        IRC::Mutex<IClient>(std::move(new_client)))
+    );
 
     /* Check if duplicate was found */
     if (ret.second == false)
