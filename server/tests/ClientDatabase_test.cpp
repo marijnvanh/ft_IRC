d90f5dcd0f9409b2eba92b3b41bf80195a63eb90
Emily Martins <emily.flakeheart@gmail.com>
2021-02-15 17:18:36 +0100

    make tests work again

--- server/tests/ClientDatabase_test.cpp
+++ server/tests/ClientDatabase_test.cpp
@@ -33,6 +33,10 @@ class ClientDatabaseTests : public ::testing::Test
     MockClient *client3;
     std::shared_ptr<ClientDatabase> client_database;
 
+    IRC::UUID uuid1 = IRC::UUIDGenerator::GetInstance().Generate();
+    IRC::UUID uuid2 = IRC::UUIDGenerator::GetInstance().Generate();
+    IRC::UUID uuid3 = IRC::UUIDGenerator::GetInstance().Generate();
+
     void SetUp() override
     {
         unique_client1 = std::make_unique<MockClient>();
@@ -42,120 +46,104 @@ class ClientDatabaseTests : public ::testing::Test
         unique_client3 = std::make_unique<MockClient>();
         client3 = unique_client3.get();
         client_database = std::make_shared<ClientDatabase>();
+
+        EXPECT_CALL(*client1, GetUUID())
+            .WillRepeatedly(Return(uuid1));
+        EXPECT_CALL(*client2, GetUUID())
+            .WillRepeatedly(Return(uuid2));
+        EXPECT_CALL(*client3, GetUUID())
+            .WillRepeatedly(Return(uuid3));
     }
 };
 
-// TEST_F(ClientDatabaseTests, AddAndRemoveClient)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     client_database->AddClient(std::move(unique_client1));
-// 
-//     client_database->RemoveClient(1);
-// }
-// 
-// TEST_F(ClientDatabaseTests, GetClientByUUID)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     client_database->AddClient(std::move(unique_client1));
-// 
-//     auto client = client_database->GetClient(1);
-//     client->Access([this](IClient &client)
-//         {
-//             ASSERT_EQ(client1, &client);
-//         });
-// }
-// 
-// TEST_F(ClientDatabaseTests, GetInvalidClientByFakeUUID)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     client_database->AddClient(std::move(unique_client1));
-// 
-//     ASSERT_THROW(client_database->GetClient(2), ClientDatabase::ClientNotFound);
-// }
-// 
-// /* This test adds three clients and expects to receive from all of them.
-// This means that the callback should have been called on all three clients */
-// TEST_F(ClientDatabaseTests, PollClients)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     EXPECT_CALL(*client2, GetUUID())
-//         .WillOnce(Return(2));
-//     EXPECT_CALL(*client3, GetUUID())
-//         .WillOnce(Return(3));
-//     client_database->AddClient(std::move(unique_client1));
-//     client_database->AddClient(std::move(unique_client2));
-//     client_database->AddClient(std::move(unique_client3));
-// 
-//     EXPECT_CALL(*client1, Receive())
-//         .WillOnce(Return("test"));
-//     EXPECT_CALL(*client2, Receive())
-//         .WillOnce(Return("test1"));
-//     EXPECT_CALL(*client3, Receive())
-//         .WillOnce(Return("test2"));
-// 
-//     int callback_count = 0;
-//     client_database->PollClients([&callback_count](int uuid, std::string message) {
-//         (void)message;
-//         (void)uuid;
-//         callback_count += 1;
-//     });
-// 
-//     EXPECT_EQ(callback_count, 3);
-// }
-// 
-// /* Test if a disconnected client gets removed from the list after polling */
-// TEST_F(ClientDatabaseTests, PollDisconnectedClient)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     client_database->AddClient(std::move(unique_client1));
-// 
-//     EXPECT_CALL(*client1, Receive())
-//         .WillOnce(Throw(IClient::Disconnected("test")));
-// 
-//     client_database->PollClients([](int uuid, std::string message) {
-//         (void)message;
-//         (void)uuid;
-//     });
-// 
-//     ASSERT_THROW(client_database->GetClient(1), ClientDatabase::ClientNotFound);
-// }
-// 
-// /* This test adds three clients and expects to call SendAll from all of them. */
-// TEST_F(ClientDatabaseTests, SendAllWithThreeClients)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     EXPECT_CALL(*client2, GetUUID())
-//         .WillOnce(Return(2));
-//     EXPECT_CALL(*client3, GetUUID())
-//         .WillOnce(Return(3));
-//     client_database->AddClient(std::move(unique_client1));
-//     client_database->AddClient(std::move(unique_client2));
-//     client_database->AddClient(std::move(unique_client3));
-// 
-//     EXPECT_CALL(*client1, SendAll());
-//     EXPECT_CALL(*client2, SendAll());
-//     EXPECT_CALL(*client3, SendAll());
-// 
-//     client_database->SendAll();
-// }
-// 
-// /* Test if a disconnected client gets removed from the list after polling */
-// TEST_F(ClientDatabaseTests, SendAllWithDisconnectedClient)
-// {
-//     EXPECT_CALL(*client1, GetUUID())
-//         .WillOnce(Return(1));
-//     client_database->AddClient(std::move(unique_client1));
-// 
-//     EXPECT_CALL(*client1, SendAll())
-//         .WillOnce(Throw(IClient::Disconnected("test")));
-// 
-//     client_database->SendAll();
-// 
-//     ASSERT_THROW(client_database->GetClient(1), ClientDatabase::ClientNotFound);
-// }
+TEST_F(ClientDatabaseTests, AddAndRemoveClient)
+{
+    client_database->AddClient(std::move(unique_client1));
+
+    ASSERT_EQ(client_database->GetClient(uuid1)->Take()->GetUUID(), uuid1);
+
+    client_database->RemoveClient(uuid1);
+
+    ASSERT_THROW(client_database->GetClient(uuid1), ClientDatabase::ClientNotFound);
+}
+
+TEST_F(ClientDatabaseTests, GetInvalidClientByFakeUUID)
+{
+    client_database->AddClient(std::move(unique_client1));
+
+    ASSERT_THROW(client_database->GetClient(uuid2), ClientDatabase::ClientNotFound);
+}
+
+/* This test adds three clients and expects to receive from all of them.
+   This means that the callback should have been called on all three clients */
+TEST_F(ClientDatabaseTests, PollClients)
+{
+    client_database->AddClient(std::move(unique_client1));
+    client_database->AddClient(std::move(unique_client2));
+    client_database->AddClient(std::move(unique_client3));
+
+    EXPECT_CALL(*client1, Receive())
+        .WillOnce(Return("test"));
+    EXPECT_CALL(*client2, Receive())
+        .WillOnce(Return("test1"));
+    EXPECT_CALL(*client3, Receive())
+        .WillOnce(Return("test2"));
+
+
+    std::unordered_map<IRC::UUID, std::string> expectations = {
+        { uuid1, "test" },
+        { uuid2, "test1" },
+        { uuid3, "test2" }
+    };
+
+    int callback_count = 0;
+    client_database->PollClients([&callback_count, &expectations](IRC::UUID uuid, std::string message) {
+        EXPECT_EQ(expectations[uuid], message);
+        callback_count += 1;
+    });
+
+    EXPECT_EQ(callback_count, 3);
+}
+ 
+/* Test if a disconnected client gets removed from the list after polling */
+TEST_F(ClientDatabaseTests, PollDisconnectedClient)
+{
+    client_database->AddClient(std::move(unique_client1));
+
+    EXPECT_CALL(*client1, Receive())
+        .WillOnce(Throw(IClient::Disconnected("test")));
+
+    client_database->PollClients([](IRC::UUID uuid, std::string message) {
+        (void)message;
+        (void)uuid;
+    });
+
+    ASSERT_THROW(client_database->GetClient(uuid1), ClientDatabase::ClientNotFound);
+}
+
+/* This test adds three clients and expects to call SendAll from all of them. */
+TEST_F(ClientDatabaseTests, SendAllWithThreeClients)
+{
+    client_database->AddClient(std::move(unique_client1));
+    client_database->AddClient(std::move(unique_client2));
+    client_database->AddClient(std::move(unique_client3));
+
+    EXPECT_CALL(*client1, SendAll());
+    EXPECT_CALL(*client2, SendAll());
+    EXPECT_CALL(*client3, SendAll());
+   
+    client_database->SendAll();
+}
+
+/* Test if a disconnected client gets removed from the list after polling */
+TEST_F(ClientDatabaseTests, SendAllWithDisconnectedClient)
+{
+    client_database->AddClient(std::move(unique_client1));
+
+    EXPECT_CALL(*client1, SendAll())
+        .WillOnce(Throw(IClient::Disconnected("test")));
+
+    client_database->SendAll();
+
+    ASSERT_THROW(client_database->GetClient(uuid1), ClientDatabase::ClientNotFound);
+}
