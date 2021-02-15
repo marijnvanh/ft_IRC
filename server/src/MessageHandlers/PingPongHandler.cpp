a4aea98e8a5121bf0c0445283201702f331e9e48
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-02-15 18:26:46 +0100

    disable ping

--- server/src/MessageHandlers/PingPongHandler.cpp
+++ server/src/MessageHandlers/PingPongHandler.cpp
@@ -2,12 +2,14 @@
 
 //TODO Change to only take IRCMessage
 //TODO Get UUID from message
+//TODO Send ping
 auto PingPongHandler(std::shared_ptr<ClientDatabase> client_database, std::string message) -> void
 {
     (void)message;
-    auto client = client_database->GetClient(0);
-    client->Access([](IClient &client)
-    {
-        client.Push(std::make_shared<std::string>("pong"));
-    });
+    (void)client_database;
+    // auto client = client_database->GetClient(0);
+    // client->Access([](IClient &client)
+    // {
+    //     client.Push(std::make_shared<std::string>("pong"));
+    // });
 }
