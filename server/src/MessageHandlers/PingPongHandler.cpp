dd9526e696e4bed89458a57d45b8c5a2546dd291
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-02-16 15:38:50 +0100

    simplify take

--- server/src/MessageHandlers/PingPongHandler.cpp
+++ server/src/MessageHandlers/PingPongHandler.cpp
@@ -3,8 +3,6 @@
 auto PingPongHandler(std::shared_ptr<ClientDatabase> client_database, Message message) -> void
 {
     auto client = client_database->GetClient(message.GetUUID());
-    client->Access([](IClient &client)
-    {
-        client.Push(std::make_shared<std::string>("pong"));
-    });
+    
+    client->Take()->Push(std::make_shared<std::string>("pong"));
 }
