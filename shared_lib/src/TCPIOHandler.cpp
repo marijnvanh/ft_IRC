ebc8ffe79a8c19d3398e7da8f3acc4c53c76cff0
Emily Martins <emily.flakeheart@gmail.com>
2021-02-19 18:13:53 +0100

    split on CRLF in TCPIOHandler

--- shared_lib/src/TCPIOHandler.cpp
+++ shared_lib/src/TCPIOHandler.cpp
@@ -31,17 +31,35 @@ auto TCPIOHandler::Send(const std::string data) -> void
     }
 }
 
+const auto SpliceToCRLF(std::string &buf) -> std::optional<std::string> {
+    // FIXME: this performs pretty badly, but the logic should be as simple as this function exposes.
+    auto f = buf.find("\r\n");
+    if (f == std::string::npos) {
+        return std::nullopt;
+    } else {
+        auto ret = buf.substr(0, f);
+        buf = buf.substr(f + 2);
+        return std::optional{ret};
+    }
+}
+
 auto TCPIOHandler::Receive() -> std::optional<std::string>
 {
     try
     {
-        auto socket_state = socket_->GetState();
-        if (socket_state == TCP::SocketState::kReadyToRead)
-            return std::optional<std::string>(socket_->Recv());
-        else if (socket_state != TCP::SocketState::kConnected)
-            throw IIOHandler::Closed("Found socket in non connected state");
-        else
-            return std::nullopt;
+        auto spliced = SpliceToCRLF(splice_buffer_);
+        if (spliced) {
+            return spliced;
+        } else {
+            auto socket_state = socket_->GetState();
+            if (socket_state == TCP::SocketState::kReadyToRead) {
+                splice_buffer_ += socket_->Recv();
+                return SpliceToCRLF(splice_buffer_);
+            } else if (socket_state != TCP::SocketState::kConnected)
+                throw IIOHandler::Closed("Found socket in non connected state");
+            else
+                return std::nullopt;
+        }
     }
     catch (TCP::Socket::WouldBlock &ex)
     {
