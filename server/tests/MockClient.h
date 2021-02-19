bd592281ea18911fe7b594b6becff34726a098e0
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-02-19 12:04:51 +0100

    add mocks for message, client and clientdatabase

---
+++ server/tests/MockClient.h
@@ -0,0 +1,19 @@
+#ifndef MOCK_CLIENT_H__
+#define MOCK_CLIENT_H__
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "IClient.h"
+
+class MockClient : public IClient {
+    public:
+
+    MOCK_METHOD1(Push, void(std::string irc_message));
+    MOCK_METHOD0(Receive, std::optional<std::string>());
+    MOCK_METHOD0(SendAll, void());
+    MOCK_CONST_METHOD0(GetUUID, const IRC::UUID&());
+    MOCK_METHOD(IClient::State, GetState, (), (const, override));
+    MOCK_METHOD(void, SetState, (IClient::State state), (override));
+};
+
+#endif
