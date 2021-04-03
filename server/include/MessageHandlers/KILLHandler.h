0cce359df6471980f604d3ee975a34fe28c2d9d2
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-04-03 22:23:05 +0200

    make all message handlers objects

--- server/include/MessageHandlers/KILLHandler.h
+++ server/include/MessageHandlers/KILLHandler.h
@@ -1,9 +1,23 @@
 #ifndef KILL_HANDLER_HPP
 #define KILL_HANDLER_HPP
 
-#include "IMessage.h"
+#include "MessageHandlers/ICommandHandler.h"
 #include "IClientDatabase.h"
+#include "Logger.h"
 
-auto KILLHandler(IClientDatabase *client_database, IMessage &message) -> void;
+class KILLHandler : public ICommandHandler
+{
+    public:
+    
+    KILLHandler(IClientDatabase *client_database);
+    ~KILLHandler();
+    
+    auto Handle(IMessage &message) -> void override;
+    
+    private:
+    IClientDatabase *client_database_;
+    Logger logger;
+
+};
 
 #endif
--- server/src/MessageHandlers/QUITHandler.cpp
+++ server/src/MessageHandlers/QUITHandler.cpp
@@ -7,9 +7,35 @@
 
 #define DEFAULT_QUIT_MESSAGE "Leaving"
 
-//TODO Still a lot of todo's here
 
-static auto GetQuitMessage(IMessage &message) -> std::string
+QUITHandler::QUITHandler(IClientDatabase *client_database) :
+    client_database_(client_database),
+    logger("QUITHandler")
+{}
+
+QUITHandler::~QUITHandler()
+{}
+
+//TODO Still a lot of todo's here
+auto QUITHandler::Handle(IMessage &message) -> void
+{
+    auto client = *(client_database_->GetClient(message.GetClientUUID()));
+
+    if (client->GetState() == IClient::State::kUnRegistered)
+    {
+        client_database_->DisconnectClient(client->GetUUID());
+    }
+    else if (client->GetType() == IClient::Type::kLocalUser)
+    {
+        DisconnectLocalUser(message);
+    }
+    else if (client->GetType() == IClient::Type::kServer)
+    {
+        DisconnectRemoteUser(client, message);
+    }
+}
+
+auto QUITHandler::GetQuitMessage(IMessage &message) -> std::string
 {
     auto params = message.GetParams();
     if (params.size() == 0)
@@ -18,18 +44,16 @@ static auto GetQuitMessage(IMessage &message) -> std::string
         return params.front();
 }
 
-static auto DisconnectLocalUser(IClientDatabase *client_database, IMessage &message) -> void
+auto QUITHandler::DisconnectLocalUser(IMessage &message) -> void
 {
     auto quit_message = GetQuitMessage(message);
     // auto local_user = std::dynamic_pointer_cast<ILocalUser>(client);
     //TODO send quit message to all local Users in all the channels from local_user.GetChannels()
     //TODO remove from IServer it is connected to
-    client_database->DisconnectClient(message.GetClientUUID());
+    client_database_->DisconnectClient(message.GetClientUUID());
 }
 
-static auto DisconnectRemoteUser(IClientDatabase *client_database,
-    IClient* remote_user,
-    IMessage &message) -> void
+auto QUITHandler::DisconnectRemoteUser(IClient* remote_user, IMessage &message) -> void
 {
     auto nickname = message.GetNickname();
     if (nickname == std::nullopt)
@@ -41,34 +65,16 @@ static auto DisconnectRemoteUser(IClientDatabase *client_database,
 
     auto quit_message = GetQuitMessage(message);
     
-    auto remote_client = client_database->GetClient(*nickname);
+    auto remote_client = client_database_->GetClient(*nickname);
     if (remote_client && (*remote_client)->GetType() != IClient::Type::kRemoteUser)
     {
         //TODO send quit message to all local Users in all the channels from user.GetChannels()
         //TODO remove from IServer it is connected to
-        client_database->DisconnectClient((*remote_client)->GetUUID());
+        client_database_->DisconnectClient((*remote_client)->GetUUID());
     }
     else
     {
         remote_user->Push(GetErrorMessage(ERR_NOSUCHNICK));
         return ;
-    }
-}
-
-auto QUITHandler(IClientDatabase *client_database, IMessage &message) -> void
-{
-    auto client = *(client_database->GetClient(message.GetClientUUID()));
-
-    if (client->GetState() == IClient::State::kUnRegistered)
-    {
-        client_database->DisconnectClient(client->GetUUID());
-    }
-    else if (client->GetType() == IClient::Type::kLocalUser)
-    {
-        DisconnectLocalUser(client_database, message);
-    }
-    else if (client->GetType() == IClient::Type::kServer)
-    {
-        DisconnectRemoteUser(client_database, client, message);
     }
 }
