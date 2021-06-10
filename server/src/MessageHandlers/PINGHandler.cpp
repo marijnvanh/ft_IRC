65a2e3abf45cf81c9c9fb655436b114a7dd54838
robkuijper <robkuijper@live.nl>
2021-06-10 20:23:44 +0200

    PING initial rework

--- server/src/MessageHandlers/PINGHandler.cpp
+++ server/src/MessageHandlers/PINGHandler.cpp
@@ -1,19 +1,99 @@
+#include "Numerics.h"
+#include "Utilities.h"
 #include "MessageHandlers/PINGHandler.h"
 
-#define PARAM_SERVER_NAME 0
+#define PARAM_ORIGIN 0
+#define PARAM_TARGET 1
 
 PINGHandler::PINGHandler(IServerConfig *server_config, IClientDatabase *client_database) :
-	CommandHandler(server_config, client_database, "PING", 1, true)
+	CommandHandler(server_config, client_database, "PING", 0, true)
 {}
 
 PINGHandler::~PINGHandler()
 {}
+
+auto PINGHandler::HandleForTarget(IClient *client, IMessage &message) -> bool
+{
+	std::string origin;
+	auto params = message.GetParams();
+	auto target_name = params[PARAM_TARGET];
+
+	/* If the target is the current server, we immediately return false. */
+	if (target_name == server_config_->GetName())
+		return (false);
+
+	/* If the target is not a server, don't continue with handling the PING. */
+	auto target = client_database_->GetServer(target_name);
+	if (!target) {
+		client->Push(GetErrorMessage(server_config_->GetName(),
+			ERR_NOSUCHSERVER, target_name));
+		return (true);
+	}
+
+	/* Make sure the prefix for the redirect message is set correctly */
+	if (client->IsServer()) {
+		if (!message.GetPrefix()) {
+			client->Push(GetErrorMessage(client->GetPrefix(),
+				ERR_NEEDMOREPARAMS, "PING"));
+			return (true);
+		}
+		origin = *(message.GetPrefix());
+	} else {
+		origin = client->GetPrefix();
+	}
+
+	if (origin.empty()) {
+		client->Push(GetErrorMessage(server_config_->GetName(),
+			ERR_NOSUCHSERVER, *(message.GetPrefix())));
+		return (true);
+	}
+
+	(*target)->Push(":" + origin + " PING " + params[PARAM_ORIGIN] +
+		" :" + target_name);
+
+	return (true);
+}
+
+auto PINGHandler::HandleForThisServer(IClient* client, IMessage &message) -> void
+{
+	std::string origin;
+	auto params = message.GetParams();
+
+	/* Make sure the origin parameter for the PONG message is set correctly */
+	if (client->IsServer()) {
+		if (message.GetPrefix())
+			origin = *(message.GetPrefix());
+		else
+			origin = client->GetPrefix();
+	} else {
+		origin = server_config_->GetName();
+	}
+
+	if (origin.empty()) {
+		client->Push(GetErrorMessage(server_config_->GetName(),
+			ERR_NOSUCHSERVER, *(message.GetPrefix())));
+		return ;
+	}
+
+	client->Push(":" + client->GetPrefix() +
+		" PONG " + origin + " :" + params[PARAM_ORIGIN]);	
+}
 
 auto PINGHandler::SafeHandle(IMessage &message) -> void
 {
 	auto client = *(client_database_->GetClient(message.GetClientUUID()));
 	auto params = message.GetParams();
 
-	client->Push(":" + server_config_->GetName() +
-		" PONG " + server_config_->GetName() + " :" + params[PARAM_SERVER_NAME]);
+	/* No origin specified, push error. */
+	if (params.size() < 1) {
+		client->Push(GetErrorMessage(server_config_->GetName(), ERR_NOORIGIN,
+			client->GetPrefix()));
+		return ;
+	}
+
+	/* Target specified, test to see if we need to try and forward */
+	if (params.size() > 1 && HandleForTarget(client, message))
+		return ;
+
+	HandleForThisServer(client, message);
 }
