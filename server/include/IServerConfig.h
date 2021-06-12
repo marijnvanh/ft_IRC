c67dfaa9ed1816c30139a10d6ac21a2775d9e08c
Marijn van Heesewijk <marijnvanh@hotmail.com>
2021-06-12 18:48:41 +0200

    Improve readme

--- server/include/IServerConfig.h
+++ server/include/IServerConfig.h
@@ -49,7 +49,6 @@ struct IServerConfig
 public:
 
 	auto SetPingTime(uint32_t ping_time) -> void { ping_time_ = ping_time; }
-	auto SetId(std::string server_id) -> void { server_id_ = server_id; }
 	auto SetName(std::string server_name) -> void { server_name_ = server_name; }
 	auto SetNetwork(std::string server_network) -> void { server_network_ = server_network; }
 	auto SetDescription(std::string server_description) -> void { server_description_ = server_description; }
@@ -59,7 +58,6 @@ public:
 	auto SetAddress(std::string server_address) -> void { server_address_ = server_address; }
 
 	auto GetPingTime() -> uint32_t { return (ping_time_); }
-	auto GetId() -> const std::string { return (server_id_); }
 	auto GetName() -> const std::string { return (server_name_); }
 	auto GetNetwork() -> const std::string { return (server_network_); }
 	auto GetDescription() -> const std::string { return (server_description_); }
@@ -79,12 +77,6 @@ protected:
 	 * @brief Determines the time (in seconds) between pinging a client to check for liveness. 
 	 */
 	uint32_t ping_time_;
-
-    /**
-     * @brief ID of the server, 3 characters long.
-	 * 1st char should be a number, 2nd and 3rd should be either number or letter.
-     */
-	std::string server_id_;
 
     /**
      * @brief Name of the server, tends to be similar (if not equal) to server_network.
